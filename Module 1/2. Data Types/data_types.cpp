43a188eee55281d566380714af53696c0d0b37d4
Suraj Mhatre <surajmcodes@gmail.com>
2021-03-21 13:03:45 +0530

    Data Types

---
+++ Module 1/2. Data Types/data_types.cpp
@@ -0,0 +1,26 @@
+//C++ Program to check size of data types
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Size of char : " << sizeof(char) << " bytes" << endl;
+
+    cout << "Size if wchar_t : " << sizeof(wchar_t) << " bytes" << endl;
+
+    cout << "Size of int : " << sizeof(int) << " bytes" << endl;
+
+    cout << "Size of short int : " << sizeof(short int) << " bytes" << endl;
+
+    cout << "Size of long int : " << sizeof(long int) << " bytes" << endl;
+
+    cout << "Size of signed long int : " << sizeof(signed long int) << " bytes" << endl;
+
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int) << " bytes" << endl;
+
+    cout << "Size of float : " << sizeof(float) << " bytes" << endl;
+
+    cout << "Size of double : " << sizeof(double) << " bytes" << endl;
+}
