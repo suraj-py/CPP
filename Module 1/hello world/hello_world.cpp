21e83bac36cd8a580548013213751efd15c9f39f
Suraj Mhatre <surajmcodes@gmail.com>
2021-03-21 12:23:56 +0530

    Hello World Program

---
+++ Module 1/hello world/hello_world.cpp
@@ -0,0 +1,13 @@
+// Hello World in C++
+
+// Header file for input output functions
+#include <iostream>
+
+//using namespace std;
+
+// Main Function
+int main(){
+
+    std::cout << "Hello World";
+    return 0;
+};
