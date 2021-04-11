144467983222d6d02c741b148f248225774fd860
Suraj Mhatre <surajmcodes@gmail.com>
2021-04-11 16:17:54 +0530

    Functions

--- 4. Functions/func_basic.cpp
+++ 4. Functions/func_basic.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
 
-// greeting function declaration
+// function declaration
 void greeting();
+void introduceMe(string name, string city, int age);
 
 void hello(){
 
-    cout << "Hello, world .. " << endl;
+    cout << "Hello, World .. !" << endl;
 }
 
 int main(){
 
+    int age;
+    string name, city;
+
     cout << "Hello from main()." << endl;
+
     hello();
     greeting();
+
+    cout << "Enter name: " << endl;
+    cin >> name;
+    cout << "Enter city: " << endl;
+    cin >> city;
+    cout << "Enter age: " << endl;
+    cin >> age;
+    introduceMe(name, city, age);
 
     return 0;
 }
@@ -21,5 +34,13 @@ int main(){
 // greeting function definition
 void greeting(){
 
-    cout << "Hello from greeting().";
+    cout << "Hello from greeting()." << endl;
+}
+
+// introduceMe function definition
+void introduceMe(string name, string city, int age=0){
+    cout << "My name is " << name << endl;
+    cout << "I'm from " << city << endl;
+    if (age != 0)
+        cout << "My age is " << age << endl;
 }
